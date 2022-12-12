b7c5ef40694ebc7b3ff99f434c0dea5e9e2c12b4
Haru-330 <haruna.330romiyaneiro@gmail.com>
2022-12-13 00:03:20 +0900

    色々部品配置+ボタン動作など

--- p2hacks/p2hacks/Itiran/ItiranViewController.swift
+++ p2hacks/p2hacks/Itiran/ItiranViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ItiranViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    // goView12ボタン押下時の処理
+    @IBAction func button(_ sender: Any) {
+            // storyboardのインスタンス取得
+            let storyboard: UIStoryboard = UIStoryboard(name: "Post", bundle: nil)
+            // 遷移先ViewControllerのインスタンス取得
+            let nextView = storyboard.instantiateViewController(withIdentifier: "Post") as! PostViewController
+            // 画面遷移
+            self.present(nextView, animated: true, completion: nil)
+        }
 
 }
 
