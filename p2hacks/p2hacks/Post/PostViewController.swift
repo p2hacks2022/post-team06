5aed942218d0e16e7311cec3fee4592a4c8309ba
Haru-330 <haruna.330romiyaneiro@gmail.com>
2022-12-18 01:23:54 +0900

    ボタンのタグでのやつができました

--- p2hacks/p2hacks/Post/PostViewController.swift
+++ p2hacks/p2hacks/Post/PostViewController.swift
@@ -5,6 +5,49 @@
 //  Created by 涌井春那 on 2022/12/12.
 //
 
+
+/*
+ //ラジオボタンを配置する
+ func set_radiobutton(num:Int){
+ let button = UIButton()
+ let center = Int(UIScreen.main.bounds.size.width / 2)  //中央の位置
+ let y = 170+45*num  //ボタン同士が重ならないようyを調整
+ button.setImage(uncheckedImage, for: UIControl.State.normal)
+ button.addTarget(self, action: #selector(butttonClicked(_:)), for: UIControl.Event.touchUpInside)
+ button.frame = CGRect(x: center - 120,
+ y: y,
+ width: 40,
+ height: 40)
+ button.tag = num
+ self.view.addSubview(button)
+ }
+ 
+ //押されたボタンの画像をcheck_on.pngに変える
+ @objc func butttonClicked(_ sender: UIButton) {
+ ChangeToUnchecked(num: CheckedButtonTag)
+ let button = sender
+ button.setImage(checkedImage, for: UIControl.State.normal)
+ CheckedButtonTag = button.tag  //check_on.pngになっているボタンのtagを更新
+ }
+ 
+ //すでにcheck_on.pngになっているボタンの画像をcheck_off.pngに変える
+ func ChangeToUnchecked(num:Int){
+ for v in view.subviews {
+ if let v = v as? UIButton, v.tag == num {
+ v.setImage(uncheckedImage, for: UIControl.State.normal)
+ }
+ }
+ }
+ 
+ 
+ override func viewDidLoad() {
+ super.viewDidLoad()
+ for i in 0..<NumberOfButtons {
+ set_radiobutton(num: i)
+ }
+ }
+ */
+
 import UIKit
 import Firebase
 import FirebaseStorageUI
@@ -14,13 +57,41 @@ var IMAGEURL = NSURL(string: "")
 class PostViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var batsuButton: UIImageView!
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var subjectText: UITextField!
-    @IBOutlet weak var hashtagText: UITextField!
     @IBOutlet weak var descriptionTextView: PlaceTextView!
+    @IBOutlet weak var subjectText: PlaceTextView!
+    
+    @IBOutlet weak var kotobaButton: UIButton!
+    @IBOutlet weak var postButton: UIButton!
+    @IBOutlet weak var humanButton: UIButton!
+    @IBOutlet weak var musicButton: UIButton!
+    @IBOutlet weak var foodButton: UIButton!
+    @IBOutlet weak var thingButton: UIButton!
+    var NumberOfButtons: Int = 5  //ボタンの数
+    var CheckedButtonTag = 0  //チェックされているボタンのタグ
     // インスタンス変数
     var DBRef:DatabaseReference!
+    var kotobaFlag: Bool = false
+    var humanFlag: Bool = false
+    var musicFlag: Bool = false
+    var foodFlag: Bool = false
+    var thingFlag: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        kotobaButton.backgroundColor = .lightGray
+        humanButton.backgroundColor = .lightGray
+        musicButton.backgroundColor = .lightGray
+        foodButton.backgroundColor = .lightGray
+        thingButton.backgroundColor = .lightGray
+        //画面の中心を表す
+        let ViewWidth = Float(UIScreen.main.bounds.size.width)
+        //アイコンを中心に表示
+        let IconWidthGap = (ViewWidth - Float(postButton.frame.width)) / 2
+        postButton.frame = CGRect.init(x: CGFloat(IconWidthGap),
+                                       y: postButton.frame.minY,
+                                       width: postButton.frame.width,
+                                       height: postButton.frame.height)
+        
         //インスタンスを作成
         DBRef = Database.database().reference()
         let reference = DBRef.child("postData")
@@ -36,7 +107,7 @@ class PostViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         //画像をセット
         imageView.sd_setImage(with: storageref)
         //表示ここまで
-
+        
         //imageViewにタップ判定をつけるためのもの
         imageView.isUserInteractionEnabled = true
         imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action:#selector(imageViewTapped(_:))))
@@ -57,55 +128,206 @@ class PostViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         self.present(picker, animated: true)
     }
     fileprivate func upload(_ date:Date) {
-            let currentTimeStampInSecond = UInt64(floor(date.timeIntervalSince1970 * 1000))
-            let storageRef = Storage.storage().reference().child("images").child("\(currentTimeStampInSecond).jpg")
+        let currentTimeStampInSecond = UInt64(floor(date.timeIntervalSince1970 * 1000))
+        let storageRef = Storage.storage().reference().child("images").child("\(currentTimeStampInSecond).jpg")
         
-            let metaData = StorageMetadata()
-            metaData.contentType = "image/jpg"
-            if let uploadData = self.imageView.image?.jpegData(compressionQuality: 0.9) {
-                storageRef.putData(uploadData, metadata: metaData) { (metadata , error) in
+        let metaData = StorageMetadata()
+        metaData.contentType = "image/jpg"
+        if let uploadData = self.imageView.image?.jpegData(compressionQuality: 0.9) {
+            storageRef.putData(uploadData, metadata: metaData) { (metadata , error) in
+                if error != nil {
+                    print("error: \(String(describing: error?.localizedDescription))")
+                }
+                storageRef.downloadURL(completion: { (url, error) in
                     if error != nil {
                         print("error: \(String(describing: error?.localizedDescription))")
                     }
-                    storageRef.downloadURL(completion: { (url, error) in
-                        if error != nil {
-                            print("error: \(String(describing: error?.localizedDescription))")
-                        }
-                        print("url: \(String(describing: url?.absoluteString))")
-                    })
-                }
+                    print("url: \(String(describing: url?.absoluteString))")
+                })
             }
         }
+    }
+    //言葉ボタンが押されたとき
+    @IBAction func chooseKotobaButton(_ sender: Any) {
+        if kotobaFlag == false {
+            //ONにする時に走らせたい処理
+            print("言葉")
+            kotobaFlag = true
+            humanFlag = false
+            thingFlag = false
+            musicFlag = false
+            foodFlag = false
+            kotobaButton.backgroundColor = UIColor(red:0.82, green:0, blue:1, alpha:1.0)
+            //UIButtonを無効化
+            humanButton.isEnabled = false
+            musicButton.isEnabled = false
+            thingButton.isEnabled = false
+            foodButton.isEnabled = false
+        } else if kotobaFlag == true {
+            //OFFにする時に走らせたい処理
+            print("言葉なし")
+            kotobaFlag = false
+            kotobaButton.backgroundColor = .lightGray
+            //UIButtonを有効化
+            humanButton.isEnabled = true
+            musicButton.isEnabled = true
+            thingButton.isEnabled = true
+            foodButton.isEnabled = true
+        }
+    }
+    @IBAction func chooseHumanButton(_ sender: Any) {
+        if humanFlag == false {
+            //ONにする時に走らせたい処理
+            print("人")
+            humanFlag = true
+            kotobaFlag = false
+            thingFlag = false
+            musicFlag = false
+            foodFlag = false
+            humanButton.backgroundColor = UIColor(red:0.82, green:0, blue:1, alpha:1.0)
+            //UIButtonを無効化
+            kotobaButton.isEnabled = false
+            musicButton.isEnabled = false
+            thingButton.isEnabled = false
+            foodButton.isEnabled = false
+        } else if humanFlag == true {
+            //OFFにする時に走らせたい処理
+            print("人なし")
+            humanFlag = false
+            humanButton.backgroundColor = .lightGray
+            //UIButtonを無効化
+            kotobaButton.isEnabled = true
+            musicButton.isEnabled = true
+            thingButton.isEnabled = true
+            foodButton.isEnabled = true
+        }
+    }
+    
+    @IBAction func chooseMusicButton(_ sender: Any) {
+        if musicFlag == false {
+            //ONにする時に走らせたい処理
+            print("曲")
+            musicFlag = true
+            kotobaFlag = false
+            humanFlag = false
+            thingFlag = false
+            foodFlag = false
+            musicButton.backgroundColor = UIColor(red:0.82, green:0, blue:1, alpha:1.0)
+            //UIButtonを無効化
+            humanButton.isEnabled = false
+            kotobaButton.isEnabled = false
+            thingButton.isEnabled = false
+            foodButton.isEnabled = false
+            
+        } else if musicFlag == true {
+            //OFFにする時に走らせたい処理
+            print("曲なし")
+            musicFlag = false
+            musicButton.backgroundColor = .lightGray
+            //UIButtonを無効化
+            humanButton.isEnabled = true
+            kotobaButton.isEnabled = true
+            thingButton.isEnabled = true
+            foodButton.isEnabled = true
+        }
+    }
+    @IBAction func chooseFoodButton(_ sender: Any) {
+        if foodFlag == false {
+            //ONにする時に走らせたい処理
+            print("食べ物")
+            foodFlag = true
+            musicFlag = false
+            kotobaFlag = false
+            humanFlag = false
+            thingFlag = false
+            foodButton.backgroundColor = UIColor(red:0.82, green:0, blue:1, alpha:1.0)
+            //UIButtonを無効化
+            humanButton.isEnabled = false
+            kotobaButton.isEnabled = false
+            thingButton.isEnabled = false
+            musicButton.isEnabled = false
+        } else if foodFlag == true {
+            //OFFにする時に走らせたい処理
+            print("食べ物")
+            foodFlag = false
+            foodButton.backgroundColor = .lightGray
+            humanButton.isEnabled = true
+            kotobaButton.isEnabled = true
+            thingButton.isEnabled = true
+            musicButton.isEnabled = true
+        }
+    }
+    @IBAction func chooseThingButton(_ sender: Any) {
+        if thingFlag == false {
+            //ONにする時に走らせたい処理
+            print("もの")
+            thingFlag = true
+            kotobaFlag = false
+            humanFlag = false
+            musicFlag = false
+            foodFlag = false
+            thingButton.backgroundColor = UIColor(red:0.82, green:0, blue:1, alpha:1.0)
+            //UIButtonを無効化
+            humanButton.isEnabled = false
+            kotobaButton.isEnabled = false
+            musicButton.isEnabled = false
+            foodButton.isEnabled = false
+        } else if thingFlag == true {
+            //OFFにする時に走らせたい処理
+            print("ものなし")
+            thingFlag = false
+            thingButton.backgroundColor = .lightGray
+            //UIButtonを無効化
+            humanButton.isEnabled = true
+            kotobaButton.isEnabled = true
+            musicButton.isEnabled = true
+            foodButton.isEnabled = true
+        }
+    }
     //投稿ボタンが押されたときの動作
     @IBAction func addPostButtonAction(_ sender: Any) {
         let dt = Date()
         let dateFormatter = DateFormatter()
         // DateFormatter を使用して書式とロケールを指定する
         dateFormatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "yMMMdHms", options: 0, locale: Locale(identifier: "ja_JP"))
-
-//        post.sorena = 0
-//        post.name = subjectText.text!
-//        post.hashtag = ""
-//        post.hashtagOptional = hashtagText.text!
-//        post.date = dateFormatter.string(from: dt)
-//        post.explanation = descriptionTextView.text!
-//        post.imageUrl = (IMAGEURL?.absoluteString)!
-//        // post.idをどんどん足していく
-//        if POSTDATA.count != 0{
-//            post.id = POSTDATA.max(ofProperty: "id")! + 1
-        //}
-//        // Realmに書き込み
-//        try! REALM.write {
-//            REALM.add(post)
-//        }
         
-
-        let data = ["name": subjectText.text!,"hashtagOptional":hashtagText.text!,"sorena":String(0),"hashtag":"","date":dateFormatter.string(from: dt),"explanation":descriptionTextView.text!,"imageUrl":(IMAGEURL?.absoluteString)!]
+        //        post.sorena = 0
+        //        post.name = subjectText.text!
+        //        post.hashtag = ""
+        //        post.hashtagOptional = hashtagText.text!
+        //        post.date = dateFormatter.string(from: dt)
+        //        post.explanation = descriptionTextView.text!
+        //        post.imageUrl = (IMAGEURL?.absoluteString)!
+        //        // post.idをどんどん足していく
+        //        if POSTDATA.count != 0{
+        //            post.id = POSTDATA.max(ofProperty: "id")! + 1
+        //}
+        //        // Realmに書き込み
+        //        try! REALM.write {
+        //            REALM.add(post)
+        //        }
+        var flag:Int = 0
+        if(kotobaFlag == true){
+            flag = 1
+        }
+        if(humanFlag == true){
+            flag = 2
+        }
+        if(thingFlag == true){
+            flag = 3
+            
+        }
+        if(musicFlag == true){
+            flag = 4
+        }
+        if(foodFlag == true){
+            flag = 5
+        }
+        let data = ["name": subjectText.text!,"hashtagOptional":"","sorena":String(0),"hashtag":String(flag),"date":dateFormatter.string(from: dt),"explanation":descriptionTextView.text!,"imageUrl":(IMAGEURL?.absoluteString)!]
         //DBRef.child("postData").childByAutoId().setValue(data)
         DBRef.child("postData/\(UInt64(floor(dt.timeIntervalSince1970 * 1000)))").setValue(data)
         upload(dt)
         subjectText.text = ""
-        hashtagText.text = ""
         descriptionTextView.text = ""
         // storyboardのインスタンス取得
         let itiranStoryboard: UIStoryboard = UIStoryboard(name: "Itiran", bundle: nil)
@@ -114,7 +336,7 @@ class PostViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         // フルスクリーンにする
         ItiranView.modalPresentationStyle = .fullScreen
         
-       let naviVc = UINavigationController(rootViewController: ItiranView)
+        let naviVc = UINavigationController(rootViewController: ItiranView)
         
         // 画面遷移
         self.present(naviVc, animated: false, completion: nil)
